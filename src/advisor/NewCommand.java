55a2925d6519d071268dc5ebe603fb8308db01e0
Robert <robert.cuda@email.cz>
2023-07-10 16:23:09 +0200

    new command

--- src/advisor/NewCommand.java
+++ src/advisor/NewCommand.java
@@ -1,5 +1,12 @@
 package advisor;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class NewCommand implements Command{
     private CommandManager cm;
 
@@ -23,12 +30,23 @@ public class NewCommand implements Command{
             return;
         }
         if (cm.isAuthorized()) {
-            System.out.println("""
-                    ---NEW RELEASES---
-                    Mountains [Sia, Diplo, Labrinth]
-                    Runaway [Lil Peep]
-                    The Greatest Show [Panic! At The Disco]
-                    All Out Life [Slipknot]""");
+            HttpResponse<String> response = cm.httpRequest(getAPIendpoint());
+            JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
+
+            for (JsonElement item : jsonResponse.getAsJsonObject("albums").getAsJsonArray("items")) {
+                StringBuilder artists = new StringBuilder();
+                for (JsonElement elem : item.getAsJsonObject().get("artists").getAsJsonArray()) {
+                    artists.append(elem.getAsJsonObject().get("name").getAsString()).append(", ");
+                }
+                System.out.println(item.getAsJsonObject().get("name").getAsString().replace("\"", ""));
+                System.out.println("[" + artists.substring(0, artists.length() - 2) + "]");
+                System.out.println(item.getAsJsonObject()
+                        .get("external_urls").getAsJsonObject()
+                        .get("spotify").getAsString());
+                System.out.println();
+
+
+            }
         } else {
             System.out.println("Please, provide access for application.");
         }
--- src/advisor/PlaylistsCommand.java
+++ src/advisor/PlaylistsCommand.java
@@ -31,20 +31,18 @@ public class PlaylistsCommand implements Command {
             System.out.println("Please provide the category name as an argument");
             return;
         }
-        if (args.length > 1) {
-            System.out.println("This command does not have arguments");
-            return;
-        }
-        this.categoryID = cm.findID(args[0]);
+
+        this.categoryID = cm.findID(String.join(" ", args));
         this.APIendpoint = "/v1/browse/categories/"+ categoryID + "/playlists";
 
         if (cm.isAuthorized()) {
             HttpResponse<String> response = cm.httpRequest(APIendpoint);
-            if (response.statusCode() == 404) {
+            if (response.body().contains("error")) {
                 String errorMSG = JsonParser.parseString(response.body()).getAsJsonObject()
                         .get("error").getAsJsonObject()
                         .get("message").getAsString();
                 System.out.println(errorMSG);
+
                 return;
             }
             JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
